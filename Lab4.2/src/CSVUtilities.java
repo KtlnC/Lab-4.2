273c727a09df381142d806d29460e915fd2237af
BT_1N3_04 <BT_1N3_04@BT_1N3_04.bktech.local>
2017-12-19 13:13:33 -0500

    pizza

---
+++ Lab4.2/src/CSVUtilities.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**Created by Kaitlyn Cao on 12/19/2017
+ * Lab 4.2 CSV Utilities*/
+
+public class CSVUtilities {
+	
+	ArrayList<String> CSVData;
+	int numColumns;
+	
+	public CSVUtilities(File csv)
+	{
+		FileReader reader = null;
+		BufferedReader filein = null;
+		try
+		{
+		 reader = new FileReader(csv);
+
+		 filein = new BufferedReader(reader);
+		 //String line = filein.readLine();
+		 
+		}
+		catch (FileNotFoundException fnfe)
+		{
+		System.out.println("ERROR: File not found!");
+		}
+	}
+}
